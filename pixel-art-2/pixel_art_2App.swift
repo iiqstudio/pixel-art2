a08426621c1db3751a183dc0a9665834201d272b
iiqstudio <ii@qstudio.org>
2025-12-16 16:01:41 +0300

    Initial Commit

---
+++ pixel-art-2/pixel_art_2App.swift
@@ -0,0 +1,17 @@
+//
+//  pixel_art_2App.swift
+//  pixel-art-2
+//
+//  Created by Илья Ярославцев on 16.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct pixel_art_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pixel-art-2Tests/pixel_art_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  pixel_art_2Tests.swift
+//  pixel-art-2Tests
+//
+//  Created by Илья Ярославцев on 16.12.2025.
+//
+
+import Testing
+@testable import pixel_art_2
+
+struct pixel_art_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
